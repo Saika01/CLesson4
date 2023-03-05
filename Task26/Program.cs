1b87936b9d4f04146bee20a4814e12b11075c5ea
Aoi <naztya2001@bk.ru>
2023-03-05 11:03:09 +0300

    task26

---
+++ Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitNumbers = DigitNumbers(number);
+Console.WriteLine($"Количество цифр в числе: {digitNumbers}");
+
+int DigitNumbers(int num)
+{
+    int count = 0;
+    while (num>0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
