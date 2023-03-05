42d3422f2946659bd6fec4cc2c61041123e5fcec
Aoi <naztya2001@bk.ru>
2023-03-05 11:12:58 +0300

    task28

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int multiplication = Multiplication(number);
+Console.WriteLine($"произведение чисел от 1 до {number} : {multiplication}");
+
+
+int Multiplication(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
