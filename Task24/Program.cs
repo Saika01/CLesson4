781cc95d3e940b7695a9734f6fe166c28fbae14a
Aoi <naztya2001@bk.ru>
2023-03-05 10:35:37 +0300

    task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Sum of num from 1 to A
+
+int SumNum(int num)
+{
+    int result =0;
+    for(int i=1; i<=num; i++)
+    {
+        result = i+i-1;
+    }
+    return result;
+}
+
+Console.WriteLine("Input number");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNum(A));
