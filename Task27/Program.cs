2ec24540dfb0a8a9b6bb3409600e919edd699526
Aoi <naztya2001@bk.ru>
2023-03-07 14:01:12 +0300

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿int Summary(int num)
+{
+    int sum = 0;
+    while (num / 10 != 0)
+    {
+        sum = num % 10 + sum;
+        num = num / 10;
+    }
+    sum = sum + num;
+    return sum;
+}
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summary = 0;
+
+if (number / 10 != 0)
+{
+    summary = Summary(number);
+} else summary = number;
+
+
+Console.WriteLine(summary);
