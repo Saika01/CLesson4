5bb5fde552a9876eb08d0069c2b169ea71bebb58
Aoi <naztya2001@bk.ru>
2023-03-07 13:45:14 +0300

    task25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿int TimesTimes(int number1, int number2)
+{
+    if (number2 == 0) return 1;
+    int output = 1;
+    for (int i = 0; i < number2; i++)
+    {
+        output = number1 * output;
+    }
+    if (output == 1) return number1;
+    return output;
+}
+
+Console.WriteLine("Input first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = TimesTimes(num1,num2);
+
+Console.WriteLine(result);
